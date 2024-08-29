2827ce6999a96b1edb285231939b360f35f6c403
siddeshg907 <121315414+siddeshg907@users.noreply.github.com>
2024-08-29 07:00:11 +0530

    new 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,40 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Register from './Pages/Register';
 import Login from './Pages/Login';
-
-import Pomodoro from './Pages/Pomodoro'; 
+import Pomodoro from './Pages/Pomodoro';
 import RPSGame from './Pages/RPSGame';
 import Home from './Pages/Home';
 
-function App() {
-  const PrivateRoute = ({ element }) => {
-    return localStorage.getItem('isAuthenticated') ? (
-      element
-    ) : (
-      <Navigate to="/login" replace />
-    );
-  };
+function PrivateRoute({ children }) {
+  return localStorage.getItem('isAuthenticated') ? (
+    children
+  ) : (
+    <Navigate to="/login" replace />
+  );
+}
 
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<PrivateRoute element={<Home />} />} />
-        <Route path="/rps-game" element={<PrivateRoute element={<RPSGame />} />} />
-        <Route path="/pomodoro" element={<PrivateRoute element={<Pomodoro />} />} />
-        <Route path="/" element={<Navigate to="/login" replace />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/home" element={
+        <PrivateRoute>
+          <Home />
+        </PrivateRoute>
+      } />
+      <Route path="/rps-game" element={
+        <PrivateRoute>
+          <RPSGame />
+        </PrivateRoute>
+      } />
+      <Route path="/pomodoro" element={
+        <PrivateRoute>
+          <Pomodoro />
+        </PrivateRoute>
+      } />
+      <Route path="/" element={<Navigate to="/login" replace />} />
+    </Routes>
   );
 }
 
