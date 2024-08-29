bd969b34ecc0ec0aeac95de7b032a8c56a0e0b99
Siddhesh Gore <121315414+siddeshg907@users.noreply.github.com>
2024-08-29 07:54:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Bind to 0.0.0.0 to allow access from any IP
+    port: 3000, // Or the port of your choice
+  },
+});
